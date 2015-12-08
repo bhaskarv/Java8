2c404ff06de59d8a887e6f74425f600fcb561d6d
bhaskarv <bhaskarv.online@gmail.com>
2015-12-08 23:40:46 +0530

    Examples on predicates

--- src/main/java/org/hw/learn/predicates/PredicateUtil.java
+++ src/main/java/org/hw/learn/predicates/PredicateUtil.java
@@ -1,5 +1,6 @@
 package org.hw.learn.predicates;
 
+import java.util.List;
 import java.util.function.Predicate;
 
 public class PredicateUtil {
@@ -10,5 +11,36 @@ public class PredicateUtil {
 			result = result.and(pred);
 		}
 		return result;
+	}
+
+	@SuppressWarnings("unchecked")
+	public static <T> T firstAllMatch(List<T> input,  Predicate<T> ... tests) {
+		Predicate<T> allMatchPred = allPassPredicate(tests);
+		for (T t : input) {
+			if (allMatchPred.test(t)) {
+				return t;
+			}
+		}
+		return null;
+	}
+	
+	@SafeVarargs
+	public static <T> Predicate<T> anyMatchPredicate(Predicate<T> ... tests) {
+		Predicate<T> result = e->false;
+		for (Predicate<T> pred : tests) {
+			result = result.or(pred);
+		}
+		return result;
+	}
+	
+	@SuppressWarnings("unchecked")
+	public static <T> T firstAnyMatch(List<T> input,  Predicate<T> ... tests) {
+		Predicate<T> allMatchPred = anyMatchPredicate(tests);
+		for (T t : input) {
+			if (allMatchPred.test(t)) {
+				return t;
+			}
+		}
+		return null;
 	}
 }
--- src/main/java/org/hw/learn/predicates/PredicatesTest.java
+++ src/main/java/org/hw/learn/predicates/PredicatesTest.java
@@ -7,14 +7,17 @@ import java.util.function.Predicate;
 import org.hw.learn.StringUtils;
 
 public class PredicatesTest {
+	@SuppressWarnings("unchecked")
 	public static void main(String[] args) {
 		List<String> employees = Arrays.asList("Rama", "Shyama", "Bheema", "Joseph", "Basheer", "Ravi","Soma");
 
-		Predicate<String> pred = PredicateUtil.allPassPredicate(str -> str.contains("a") 
-																//,str -> str.length() == 4
+		Predicate<String> pred = PredicateUtil.allPassPredicate(str -> str.contains("B") 
+																,str -> str.length() > 5
 																//,str -> str.startsWith("R")
 																);
 		
 		System.out.println(StringUtils.allMatches(employees, pred));
+		System.out.println(PredicateUtil.firstAllMatch(employees, pred));
+		System.out.println(PredicateUtil.firstAnyMatch(employees, pred));
 	}
 }
