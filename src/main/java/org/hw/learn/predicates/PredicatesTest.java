1dd0b8034cd3daaf82a2bf079ccfdafd07039185
bhaskarv <bhaskarv.online@gmail.com>
2015-12-08 23:46:29 +0530

    Change in input

--- src/main/java/org/hw/learn/predicates/PredicatesTest.java
+++ src/main/java/org/hw/learn/predicates/PredicatesTest.java
@@ -9,12 +9,13 @@ import org.hw.learn.StringUtils;
 public class PredicatesTest {
 	@SuppressWarnings("unchecked")
 	public static void main(String[] args) {
-		List<String> employees = Arrays.asList("Rama", "Shyama", "Bheema", "Joseph", "Basheer", "Ravi","Soma");
+		List<String> employees = Arrays.asList("Rama", "Big Goal", "Shyama", "Bheema", "Joseph", "Basheer", "Ravis","Soma");
 
-		Predicate<String> pred = PredicateUtil.allPassPredicate(str -> str.contains("B") 
-																,str -> str.length() > 5
-																//,str -> str.startsWith("R")
-																);
+		Predicate<String> pred = PredicateUtil.
+								allPassPredicate(str -> str.contains("i"),
+									str -> str.length() >= 5
+									// ,str -> str.startsWith("R")
+								 );
 		
 		System.out.println(StringUtils.allMatches(employees, pred));
 		System.out.println(PredicateUtil.firstAllMatch(employees, pred));
